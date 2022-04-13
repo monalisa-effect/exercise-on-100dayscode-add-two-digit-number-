d8f6f4c037595fde6c53e7546c266aa4aae0edeb
Monalisa Egobishi <55362351+monalisa-effect@users.noreply.github.com>
2022-04-13 07:21:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+
+
+new_num1 = two_digit_number[0]
+new_num2 = two_digit_number[1]
+
+new_num1a = int(new_num1)
+new_num2a = int(new_num2)
+
+new_num3 = new_num1a + new_num2a
+
+
+print(new_num3)
+
+
+
+
